06f23885ba4a5c0e418645b3022262491dc2309c
Aidan <aidanmcloughlinatx@gmail.com>
2026-05-11 19:53:29 -0500

    finished documentation

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,17 +1,20 @@
 const browserTheme = 'theme';
 
+// Checks the browser's local storage for the user's theme preference, or defaults to 'light' if not set 
 function getTheme() {
   	return localStorage.getItem(browserTheme) || 'light';
 }
 
+// Applies the theme, then saves it
 function applyTheme(theme) {
-	document.documentElement.classList.toggle(
+	document.documentElement.classList.toggle( // Adds or removes the 'dark' class on the stylesheet
 		'dark',
 		theme === 'dark'
 	);
-  	localStorage.setItem(browserTheme, theme);
+  	localStorage.setItem(browserTheme, theme); // Save the user's theme preference in local storage
 }
 
+// Switches the theme to the opposite of the current theme
 function toggleTheme() {
   	const currentTheme = getTheme();
   	applyTheme(currentTheme === 'dark' ? 'light' : 'dark');
@@ -19,6 +22,6 @@ function toggleTheme() {
 
 applyTheme(getTheme());
 
-// Add event listener to the dark/light mode button to toggle between the themes on click
+// Adds event listener to the theme toggle button
 const button = document.getElementById('themeToggle');
 button?.addEventListener('click', toggleTheme);
